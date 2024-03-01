cb436933fda3b77374c5d3fb31ca9f01ac99d95d
Mohammed Usama <usamabhakrani@gmail.com>
2024-03-01 17:25:52 +0500

    mongoose pre middleware for hashing passwords

--- models/User.js
+++ models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
 const userSchema = new mongoose.Schema({
@@ -22,6 +23,12 @@ const userSchema = new mongoose.Schema({
     required: [true, "Please Provide a Name"],
     minlength: 6,
   },
+});
+
+userSchema.pre("save", async function (next) {
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
 });
 
 module.exports = mongoose.model("User", userSchema);
