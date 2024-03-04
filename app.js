c9a188b9d41b58676bb031f40c391ac542df66d8
Mohammed Usama <usamabhakrani@gmail.com>
2024-03-04 17:48:36 +0500

    implement auth middleware

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require("express");
 const app = express();
 const authRouter = require("./routes/auth");
 const jobsRouter = require("./routes/jobs");
+const authenticateuser = require("./middleware/authentication");
 
 // error handler
 const notFoundMiddleware = require("./middleware/not-found");
@@ -15,7 +16,7 @@ app.use(express.json());
 
 // routes
 app.use("/api/v1/auth", authRouter);
-app.use("/api/v1/jobs", jobsRouter);
+app.use("/api/v1/jobs", authenticateuser, jobsRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
